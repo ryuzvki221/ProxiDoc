85df55df6111fc705afb427d25111494a95743ed
cheikhtall <cheikhtall1199@gmail.com>
2022-06-16 16:37:16 +0000

    test login

--- src/authentification/forms.py
+++ src/authentification/forms.py
@@ -1,4 +1,4 @@
-from django import forms    
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={ 
+            attrs={
                 "placeholder": "username",
                 "class": "form-control"
             }
@@ -14,7 +14,7 @@ class LoginForm(forms.Form):
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
-            attrs={ 
+            attrs={
                 "placeholder": "password",
                 "class": "form-control"
             }
@@ -56,7 +56,7 @@ class RegisterForm(UserCreationForm):
         )
     )
 
+
 class Meta:
     model = User
     fields = ('username', 'email', 'password1', 'password2')
-
