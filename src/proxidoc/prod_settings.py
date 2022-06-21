1072177ff4fba038def723ad8a6f18061ee25414
ryuzvki221 <contact.stateless@gmail.com>
2022-06-21 06:10:16 +0000

    edit production settings

--- src/proxidoc/prod_settings.py
+++ src/proxidoc/prod_settings.py
@@ -4,4 +4,4 @@ from .settings import *
 SECRET_KEY = '@$xy!roylbi23^#cxl$#je2q^oty8rqap897lz5@abr$8@xqt*'
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'proxidoc.herokuapp.com']
+ALLOWED_HOSTS = ['proxidoc.herokuapp.com']
