1df184f62319033550094f451f61d3c3598a8867
ryuzvki221 <contact.stateless@gmail.com>
2022-06-21 08:53:08 +0000

    DEBUG production settings

--- src/proxidoc/prod_settings.py
+++ src/proxidoc/prod_settings.py
@@ -1,7 +1,18 @@
-from .settings import *
+from settings import *
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'c*6zjk#s&!&$s-!djeycl772hbs4%m8js2$u&n$h9y&nsk&e(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = '@$xy!roylbi23^#cxl$#je2q^oty8rqap897lz5@abr$8@xqt*'
 DEBUG = False
+
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['proxidoc.herokuapp.com']
+
+# Application definition
