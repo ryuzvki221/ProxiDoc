bf23c3e4ee349a582b1f111eee2d0d6a01c62c19
ryuzvki221 <contact.stateless@gmail.com>
2022-06-22 13:55:30 +0000

    add profile page

--- src/authentification/views.py
+++ src/authentification/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm, RegisterForm
 
 
@@ -15,11 +15,11 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("/")
+                return redirect("profile")
             else:
-                msg = 'Invalid Credentials'
+                msg = 'Identifiants non valides'
         else:
-            msg = 'Error validating the form'
+            msg = 'Erreur de validation du formulaire'
     return render(request, 'auth/account/login.html', {"form": form, "msg": msg})
 
 
@@ -36,11 +36,11 @@ def register_view(request):
             raw_password = form.cleaned_data.get('password')
             authenticate(username=username, password=raw_password)
 
-            msg = 'User created - please <a href="/login">login</a>'
+            msg = 'Utilisateur créé -  <a href="/login">se connecter</a>'
             success = True
             # return redirect ("/auth/login/")
         else:
-            msg = 'Form is not valid'
+            msg = "Le formulaire n'est pas valide"
     else:
         form = RegisterForm()
 
@@ -59,5 +59,12 @@ def error500_view(request):
     return render(request, 'auth/500.html')
 
 
+# Create logout view
+def logout_view(request):
+    logout(request)
+    return redirect("login")
 
 
+# Create profile view
+def profile_view(request):
+    return render(request, 'auth/account/profile.html')
